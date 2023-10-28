dc4ab31c63531e01ac8267a88d182365ae32c925
Guhan M <116095658+guhan0000@users.noreply.github.com>
2023-10-28 22:31:43 +0530

    Update asc.sql

--- asc.sql
+++ asc.sql
@@ -1,11 +1,11 @@
-#select * from product;
-#desc product
-#alter table product add column mftr varchar(20);
-#select * from product;
-#alter table product rename column mfgd to mfgdate; 
-#select * from product;
-#alter table product modify column mftr date after p_name;
-#alter table product modify column  mftr varchar(20);
-#alter table product add column email varchar(25);
-#alter table product modify column email varchar(25) first;
-#alter table product drop column email;
+select * from product;
+desc product
+alter table product add column mftr varchar(20);
+select * from product;
+alter table product rename column mfgd to mfgdate; 
+select * from product;
+alter table product modify column mftr date after p_name;
+alter table product modify column  mftr varchar(20);
+alter table product add column email varchar(25);
+alter table product modify column email varchar(25) first;
+alter table product drop column email;
